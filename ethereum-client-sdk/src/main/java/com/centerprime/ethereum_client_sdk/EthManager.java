24691bff7b770f6466fb0582cce770daf2d5c930
Odiljon <odil_ergashev_1994@mail.ru>
2020-11-18 12:10:34 +0900

    -- minor changes

--- ethereum-client-sdk/src/main/java/com/centerprime/ethereum_client_sdk/EthManager.java
+++ ethereum-client-sdk/src/main/java/com/centerprime/ethereum_client_sdk/EthManager.java
@@ -56,6 +56,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 /**
  * Created by CenterPrime on 2020/09/19.
  */
+
 public class EthManager {
     private static final EthManager ourInstance = new EthManager();
 
