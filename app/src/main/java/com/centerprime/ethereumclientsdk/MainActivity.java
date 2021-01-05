b825c7944ffd2334622cc3535b04fb0c475773f9
centerprime <Happy0990!@#>
2021-01-05 14:32:38 +0900

    -- adding minor changes and cleaning code

--- app/src/main/java/com/centerprime/ethereumclientsdk/MainActivity.java
+++ app/src/main/java/com/centerprime/ethereumclientsdk/MainActivity.java
@@ -29,49 +29,10 @@ public class MainActivity extends AppCompatActivity {
                     String walletAddress = wallet.getAddress();
                     String keystore = wallet.getKeystore();
                     Toast.makeText(this, wallet.getAddress(), Toast.LENGTH_SHORT).show();
-                    System.out.println("****************"+wallet.getAddress());
-                    a(walletAddress);
                 }, error -> {
                     error.printStackTrace();
                 });
 
-//        a("0x115ec0771d5715fe2c0b61ce0c2cb2a20e4c683d");
-
-
-
     }
-    public void a(String walletAddress){
-        ethManager.getTokenBalance(walletAddress, "xxxx12345", "0x913903bD683914288FDaa812cC2f51F243cCC731", this)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(tx -> {
 
-                    Toast.makeText(this, "TX token balance : " + tx, Toast.LENGTH_SHORT).show();
-
-                }, error -> {
-                    Toast.makeText(this, error.getMessage(), Toast.LENGTH_SHORT).show();
-                    System.out.println(error.getMessage());
-                });
-
-
-
-/*        String password1 = "xxxx12345";
-        BigInteger gasPrice = new BigInteger("30000000000");
-        BigInteger gasLimit = new BigInteger("100000");
-        BigDecimal tokenAmount = new BigDecimal("100");
-        String receiverAddress = "0x37eb5b96dbe7eb691d450e84c632c21cf2ac858d";
-        String erc20TokenContractAddress = "0x913903bD683914288FDaa812cC2f51F243cCC731";
-        ethManager.sendToken(walletAddress, password1, gasPrice, gasLimit, tokenAmount, receiverAddress, erc20TokenContractAddress, this)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(tx -> {
-
-                    Toast.makeText(this, "TX : " + tx, Toast.LENGTH_SHORT).show();
-
-                }, error -> {
-                    Toast.makeText(this, error.getMessage(), Toast.LENGTH_SHORT).show();
-                    System.out.println(error.getMessage());
-                });*/
-
-    }
 }
